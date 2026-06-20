255ae3e319686e9e3204882f375144b67cd0c022
HI <prashanthbanoth23@gmail.com>
2026-06-20 14:20:14 +0530

    Reset and create jobs

--- jobs/views.py
+++ jobs/views.py
@@ -226,8 +226,8 @@ def my_saved_jobs(request):
 
 
 def create_sample_jobs(request):
-    if Job.objects.exists():
-        return HttpResponse("Jobs already exist")
+
+    Job.objects.all().delete()
     Job.objects.create(
         title="Python Backend Developer",
         company="Infosys",
